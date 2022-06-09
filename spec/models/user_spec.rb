cbd7fcd28f27c4f35ec26bea3ae3c5bd962ce39f
inoue-ryoki <mth.1021@icloud.com>
2022-06-09 20:00:19 +0900

    生年月日の名前の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('First name reading is invalid')
       end
-      it '生年月日が空では登録できない' do
+      it 'birthdayが空では登録できない' do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
